d5daa2675fff076eeacab3a1f34ea9219375b333
dem_1 <dem_1@DESKTOP-VV2G1UE>
2023-04-20 00:24:35 +0300

    Добавьте файлы проекта.

---
+++ passwordGenerator/Program.cs
@@ -0,0 +1,23 @@
+﻿namespace passwordGenerator
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write(" [>] Enter password lenght: ");
+            var lenght = Convert.ToInt32(Console.ReadLine());
+
+            var sym = @"QWERTYUIOPASDFGHJKLZXCVBNMqwertyuiop[]asdfghjkl;'\zxcvbnm,./1234567890-=!@#$%^&*()_+";
+            Random random = new Random();
+            var stringChars = new char[lenght];
+
+            for (int i = 0; i < stringChars.Length; i++)
+            {
+                stringChars[i] = sym[random.Next(sym.Length)];
+            }
+
+            var finalString = new String(stringChars);
+            Console.WriteLine($" [*] Password: {finalString}");
+        }
+    }
+}
